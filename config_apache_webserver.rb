568dff9601dd69f48ed791e6ecd0a2134da96e53
novicejava1 <sudhirbhoga@gmail.com>
2017-06-06 01:45:13 -0400

    configure apache webserver using chef

---
+++ config_apache_webserver.rb
@@ -0,0 +1,15 @@
+package 'Install Apache' do
+	case node[:platform]
+	when 'redhat','centos'
+		package_name 'httpd'
+		action	:install
+	when 'ubuntu','debian'
+		package_name 'apache2'
+		action :install
+	end
+end
+
+service 'httpd' do
+	action [:enable, :start]
+end
+
