81efda288a585c37d99df478bde1c0738da214f8
novicejava1 <sudhirbhoga@gmail.com>
2017-06-06 05:09:44 -0400

    updated the apache_httpd cookbook with user and group add

--- cookbooks/apache_httpd/recipes/default.rb
+++ cookbooks/apache_httpd/recipes/default.rb
@@ -14,10 +14,21 @@ package 'Install Apache' do
                 end
 end
 
+group 'web_admin'
+
+user 'web_admin' do
+	group 'web_admin'
+	system true
+	shell '/bin/bash'
+end
+
 service 'httpd' do
                 action [:enable, :start]
 end
 
 template '/var/www/html/index.html' do
                 source 'index.html.erb'
+		mode '0644'
+		owner 'web_admin'
+		group 'web_admin'
 end
